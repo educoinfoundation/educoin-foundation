3db9c1f5b15346dcf5a2ee14b06b57bc4e8499dc
pisano <leonardo.pisano.bigollo.1170@gmail.com>
2014-04-27 16:39:00 +0900

    KGW TW Fixed. Hadrfork at block 140000

--- src/main.cpp
+++ src/main.cpp
@@ -917,7 +917,7 @@ unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const CBloc
 	double EventHorizonDeviationSlow;
 
 	if (BlockLastSolved == NULL || BlockLastSolved->nHeight == 0 || (uint64)BlockLastSolved->nHeight < PastBlocksMin) { return bnProofOfWorkLimit.GetCompact(); }
-
+	int64 LatestBlockTime = BlockLastSolved->GetBlockTime();
 	for (unsigned int i = 1; BlockReading && BlockReading->nHeight > 0; i++) {
             if (PastBlocksMax > 0 && i > PastBlocksMax) { break; }
             PastBlocksMass++;
@@ -926,10 +926,20 @@ unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const CBloc
             else                { PastDifficultyAverage = ((CBigNum().SetCompact(BlockReading->nBits) - PastDifficultyAveragePrev) / i) + PastDifficultyAveragePrev; }
             PastDifficultyAveragePrev = PastDifficultyAverage;
             
-            PastRateActualSeconds                        = BlockLastSolved->GetBlockTime() - BlockReading->GetBlockTime();
+            if (LatestBlockTime < BlockReading->GetBlockTime()) {
+              if (BlockReading->nHeight > 140000) { LatestBlockTime = BlockReading->GetBlockTime(); }
+            }
+            PastRateActualSeconds = LatestBlockTime - BlockReading->GetBlockTime();
+
             PastRateTargetSeconds                        = TargetBlocksSpacingSeconds * PastBlocksMass;
             PastRateAdjustmentRatio                        = double(1);
-            if (PastRateActualSeconds < 0) { PastRateActualSeconds = 0; }
+
+            if (BlockReading->nHeight > 140000) {
+              if (PastRateActualSeconds < 1) { PastRateActualSeconds = 1; }
+            } else {
+              if (PastRateActualSeconds < 0) { PastRateActualSeconds = 0; }
+            }
+
             if (PastRateActualSeconds != 0 && PastRateTargetSeconds != 0) {
             PastRateAdjustmentRatio                        = double(PastRateTargetSeconds) / double(PastRateActualSeconds);
             }
